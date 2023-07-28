32f07e03d51f51706d2aca4ac37fd37e3986e031
Asyl Jumagaliyev <ajumagal@jaguarlandrover.com>
2023-07-28 13:31:20 +0100

    refactor main class name

--- src/main/java/com/ajs/fakeservice/Application.java
+++ src/main/java/com/ajs/fakeservice/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FakeserviceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FakeserviceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
