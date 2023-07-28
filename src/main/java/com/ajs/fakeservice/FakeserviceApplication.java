38cb0abc5a75a719455dcc3d79fcb4596dad0072
Asyl Jumagaliyev <ajumagal@jaguarlandrover.com>
2023-07-28 13:24:01 +0100

    Initial commit

---
+++ src/main/java/com/ajs/fakeservice/FakeserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ajs.fakeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajs/fakeservice/FakeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajs.fakeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
