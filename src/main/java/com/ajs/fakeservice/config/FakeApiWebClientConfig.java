effb6b875750b645794535a0af1bbd7868d851f1
Asyl Jumagaliyev <ajumagal@jaguarlandrover.com>
2023-07-28 14:30:21 +0100

    refactor web client creation

--- src/main/java/com/ajs/fakeservice/config/FakeApiWebClientConfig.java
+++ src/main/java/com/ajs/fakeservice/config/FakeApiWebClientConfig.java
@@ -2,6 +2,7 @@ package com.ajs.fakeservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -11,12 +12,11 @@ public class FakeApiWebClientConfig {
 
   @Bean
   public FakeApiClient fakeApiWebClient(final FakeApiConfig fakeApiConfig) {
-      WebClient webClient = WebClient.builder()
-            .baseUrl(fakeApiConfig.getBaseUrl())
-            .build();
-      HttpServiceProxyFactory proxyFactory = HttpServiceProxyFactory
-            .builder(WebClientAdapter.forClient(webClient))
-            .build();
-      return proxyFactory.createClient(FakeApiClient.class);
-    }
+    return HttpServiceProxyFactory
+          .builder(WebClientAdapter.forClient(WebClient.builder()
+                .baseUrl(fakeApiConfig.getBaseUrl())
+                .defaultHeaders(headers -> headers.setContentType(MediaType.APPLICATION_JSON))
+                .build()))
+          .build().createClient(FakeApiClient.class);
+  }
 }
